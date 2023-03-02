4325022a461604e63cfed3b3065bd511285ff177
wuzhdcm <1360520870@qq.com>
2023-03-02 23:03:34 +0800

    idea first GitTest

---
+++ src/main/java/com/hnkjxy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hnkjxy.git;
+
+/**
+ * @author wuzihao
+ * @date 2023/3/2 - 22:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
