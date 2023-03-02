50bef4ab4c09b3bed6a4471f7ba1ce23fc996e12
wuzhdcm <1360520870@qq.com>
2023-03-02 23:23:40 +0800

    idea first GitTest

--- src/main/java/com/hnkjxy/git/GitTest.java
+++ src/main/java/com/hnkjxy/git/GitTest.java
@@ -7,5 +7,9 @@ package com.hnkjxy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
+        System.out.println("444");
     }
 }
